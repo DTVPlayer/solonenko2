99673c3ce8b6738272c76d20d44ea8917a49cf69
DTVPlayer <127330635+DTVPlayer@users.noreply.github.com>
2024-09-27 13:13:50 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,131 @@
-let gameChar_x = 100;
-let gameChar_y = 200;
-
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-  
-  // Standing front facing
-  let standBoxX = 50;
-  let standBoxY = 150;
-  rect(standBoxX, standBoxY, 50, 100); // Body
-  ellipse(standBoxX + 25, standBoxY - 20, 50, 50); // Head
-  
-  // Jumping facing forwards
-  let jumpBoxX = 150;
-  let jumpBoxY = 100;
-  rect(jumpBoxX, jumpBoxY, 50, 100); // Body
-  ellipse(jumpBoxX + 25, jumpBoxY - 30, 50, 50); // Head
-  line(jumpBoxX, jumpBoxY + 100, jumpBoxX + 25, jumpBoxY + 100); // Legs
-  
-  // Walking left
-  let walkLeftBoxX = 250;
-  let walkLeftBoxY = 150;
-  rect(walkLeftBoxX, walkLeftBoxY, 50, 100); // Body
-  ellipse(walkLeftBoxX + 25, walkLeftBoxY - 20, 50, 50); // Head
-  line(walkLeftBoxX, walkLeftBoxY + 100, walkLeftBoxX - 20, walkLeftBoxY + 110); // Left leg
-  line(walkLeftBoxX, walkLeftBoxY + 100, walkLeftBoxX + 20, walkLeftBoxY + 100); // Right leg
-  
-  // Walking right
-  let walkRightBoxX = 350;
-  let walkRightBoxY = 150;
-  rect(walkRightBoxX, walkRightBoxY, 50, 100); // Body
-  ellipse(walkRightBoxX + 25, walkRightBoxY - 20, 50, 50); // Head
-  line(walkRightBoxX, walkRightBoxY + 100, walkRightBoxX + 20, walkRightBoxY + 110); // Right leg
-  line(walkRightBoxX, walkRightBoxY + 100, walkRightBoxX, walkRightBoxY + 100); // Left leg
-  
-  // Jumping left
-  let jumpLeftBoxX = 50;
-  let jumpLeftBoxY = 50;
-  rect(jumpLeftBoxX, jumpLeftBoxY, 50, 100); // Body
-  ellipse(jumpLeftBoxX + 25, jumpLeftBoxY - 30, 50, 50); // Head
-  line(jumpLeftBoxX, jumpLeftBoxY + 100, jumpLeftBoxX - 10, jumpLeftBoxY + 110); // Left leg
-  
-  // Jumping right
-  let jumpRightBoxX = 150;
-  let jumpRightBoxY = 50;
-  rect(jumpRightBoxX, jumpRightBoxY, 50, 100); // Body
-  ellipse(jumpRightBoxX + 25, jumpRightBoxY - 30, 50, 50); // Head
-  line(jumpRightBoxX, jumpRightBoxY + 100, jumpRightBoxX + 20, jumpRightBoxY + 110); // Right leg
-}
+    // The attributes of the player.
+    var player = {
+        x: 200,
+        y: 200,
+        x_v: 0,
+        y_v: 0,
+        jump : true,
+        height: 20,
+        width: 20
+        };
+    // The status of the arrow keys
+    var keys = {
+        right: false,
+        left: false,
+        up: false,
+        };
+    // The friction and gravity to show realistic movements    
+    var gravity = 0.6;
+    var friction = 0.7;
+    // The number of platforms
+    var num = 2;
+    // The platforms
+    var platforms = [];
+    // Function to render the canvas
+    function rendercanvas(){
+        ctx.fillStyle = "#F0F8FF";
+        ctx.fillRect(0, 0, 270, 270);
+    }
+    // Function to render the player
+    function renderplayer(){
+        ctx.fillStyle = "#F08080";
+        ctx.fillRect((player.x)-20, (player.y)-20, player.width, player.height);
+        }
+    // Function to create platforms
+    function createplat(){
+        for(i = 0; i < num; i++) {
+            platforms.push(
+                {
+                x: 100 * i,
+                y: 200 + (30 * i),
+                width: 110,
+                height: 15
+                }
+            );
+        }
+        }
+    // Function to render platforms
+    function renderplat(){
+        ctx.fillStyle = "#45597E";
+        ctx.fillRect(platforms[0].x, platforms[0].y, platforms[0].width, platforms[0].height);
+        ctx.fillRect(platforms[1].x, platforms[1].y, platforms[1].width,platforms[1]. height);
+    
+    }
+    // This function will be called when a key on the keyboard is pressed
+    function keydown(e) {
+        // 37 is the code for the left arrow key
+        if(e.keyCode == 37) {
+            keys.left = true;
+        }
+        // 37 is the code for the up arrow key
+        if(e.keyCode == 38) {
+            if(player.jump == false) {
+                player.y_v = -10;
+            }
+        }
+        // 39 is the code for the right arrow key
+        if(e.keyCode == 39) {
+            keys.right = true;
+        }
+    }
+    // This function is called when the pressed key is released
+    function keyup(e) {
+        if(e.keyCode == 37) {
+            keys.left = false;
+        }
+        if(e.keyCode == 38) {
+            if(player.y_v < -2) {
+            player.y_v = -3;
+            }
+        }
+        if(e.keyCode == 39) {
+            keys.right = false;
+        }
+    } 
+    function loop() {
+        // If the player is not jumping apply the effect of frictiom
+        if(player.jump == false) {
+            player.x_v *= friction;
+        } else {
+            // If the player is in the air then apply the effect of gravity
+            player.y_v += gravity;
+        }
+        player.jump = true;
+        // If the left key is pressed increase the relevant horizontal velocity
+        if(keys.left) {
+            player.x_v = -2.5;
+        }
+        if(keys.right) {
+            player.x_v = 2.5;
+        }
+        // Updating the y and x coordinates of the player
+        player.y += player.y_v;
+        player.x += player.x_v;
+        // A simple code that checks for collions with the platform
+        let i = -1;
+        if(platforms[0].x < player.x && player.x < platforms[0].x + platforms[0].width &&
+        platforms[0].y < player.y && player.y < platforms[0].y + platforms[0].height){
+            i = 0;
+        }
+        if(platforms[1].x < player.x && player.x < platforms[1].x + platforms[1].width &&
+        platforms[1].y < player.y && player.y < platforms[1].y + platforms[1].height){
+            i = 1;
+        }
+        if (i > -1){
+            player.jump = false;
+            player.y = platforms[i].y;    
+        }
+        // Rendering the canvas, the player and the platforms
+        rendercanvas();
+        renderplayer();
+        renderplat();
+    }
+    canvas=document.getElementById("canvas");
+    ctx=canvas.getContext("2d");
+    ctx.canvas.height = 270;
+    ctx.canvas.width = 270;
+    createplat();
+    // Adding the event listeners
+    document.addEventListener("keydown",keydown);
+    document.addEventListener("keyup",keyup);
+    setInterval(loop,22);
